b801e498ac8e273e13413afa3892829575cc715b
elysium09 <eyueldaniel0921@gmail.com>
2022-01-06 17:12:55 +0300

    :rocket: MaxDepthOfBinaryTree

---
+++ src/com/codexo/trees/MaxDepthOfBinaryTree.java
@@ -0,0 +1,30 @@
+package com.codexo.trees;
+
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+public class MaxDepthOfBinaryTree {
+
+    /**
+     * Time Complexity: O(n) Linear
+     * Space Complexity: O(n) Linear
+     */
+    public static int maxDepth(TreeNode root) {
+        return maxDepth(root, 0);
+    }
+
+    private static int maxDepth(TreeNode node, int currentDepth) {
+        if (node == null) return currentDepth;
+        currentDepth++;
+        return Math.max(maxDepth(node.right, currentDepth), maxDepth(node.left, currentDepth));
+    }
+
+    public static void main(String[] args) {
+        TreeNode tree = new TreeNode(3,
+                new TreeNode(9),
+                new TreeNode(20,
+                        new TreeNode(15),
+                        new TreeNode(7))
+        );
+
+        System.out.println(maxDepth(tree)); // 3
+    }
+}
